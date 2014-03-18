1d0ee1ab66417928de17266aa4216f5dc5688485
Mayank Gupta <mayankrocks.1993@gmail.com>
2014-03-18 16:51:18 +0530

    acronym crawler added

---
+++ acronym/getAcronym.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+baseUrl="http://www.noslang.com/dictionary/"
+page=list(map(chr, range(97, 123)))
+completeList=[]
+f=open("acronyn.txt",'w')
+e=open("error.txt",'w')
+for i in page:
+    print i
+    url=baseUrl+i
+    r  = requests.get(url)
+    data = r.text
+    soup = BeautifulSoup(data)
+    if i!='j' and i!='t':
+        acr = soup.findAll('strong')
+        acr=[i.string for i in acr]
+        abr = soup.findAll('dd')
+        abr=[i.string for i in abr]
+        size=len(abr)
+        for i in range(size):
+            try:
+                print str(acr[i])+','+str(abr[i])
+                f.write(str(acr[i])+','+str(abr[i])+'\n');
+            except:
+                print str(acr[i])
+                e.write(str(acr[i])+'\n');
+    else:
+        comb=soup.findAll('abbr')
+        print comb
+        comb=[ (str(i.string),str(i['title'])) for i in comb ]
+        for i in comb:
+            try:
+                print str(i[0])+','+str(i[0])
+                f.write(str(i[0])+','+str(i[0])+'\n');
+            except:
+                print str(i[0])
+                e.write(str(i[0])+'\n');

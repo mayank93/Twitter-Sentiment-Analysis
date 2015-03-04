b85c1bf2ee00fc3bed608780310fea5b68adda6d
Mayank Gupta <mayankrocks.1993@gmail.com>
2015-03-04 20:11:54 +0530

    managing data

--- front-end/web2py/parameters_8000.py
+++ front-end/web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$830dc3aa78ad7102$3c66857b478f87e710c1c71ec333dae7135ee58a"
+password="pbkdf2(1000,20,sha512)$9b306021ef86de1c$e61cbeed3794dfc84b7d26acec518e6c4c9441bf"
